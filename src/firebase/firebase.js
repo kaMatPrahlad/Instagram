8e7d0ec80cd85165758d4dd4796846fdd04d1198
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-07 00:51:16 +0530

    git init

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,3 @@
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-//   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-// };
-
 // Import the functions you need from the SDKs you need
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
@@ -15,13 +5,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSFxe544o87EAOxOLo0cQT24QyMgW9JQc",
-  authDomain: "insta-clone-756af.firebaseapp.com",
-  projectId: "insta-clone-756af",
-  storageBucket: "insta-clone-756af.appspot.com",
-  messagingSenderId: "820026503310",
-  appId: "1:820026503310:web:422147c182fc65c494d9df",
-  measurementId: "G-F64DVQQEB9",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -30,25 +20,3 @@ const firestore = getFirestore(app);
 const storage = getStorage(app);
 
 export { app, auth, firestore, storage };
-
-
-
-
-// cloud firestore example
-// rules_version = '2';
-
-// service cloud.firestore {
-//   match /databases/{database}/documents {
-//   match /users/{userId}{
-//   allow read;
-//   allow write: if request.auth != null && request.auth.uid == userId;
-//   }
-//   match/posts/{postId}{
-//  allow read;
-//  allow create: if request.auth != null;
-//  allow update: if request.auth != null && request.auth.uid == resource.data.createdBY;
-//  allow delete: if request.auth != null && request.auth.uid == resource.data.createdBY;
-// }
-
-//   }
-// }
