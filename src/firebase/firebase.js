aff57576043494685e3466ffd35a9233acd3ef25
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-07 00:22:06 +0530

    git init

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,8 +1,3 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import { getStorage } from "firebase/storage";
-
 // const firebaseConfig = {
 //   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
 //   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -13,10 +8,47 @@
 //   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 // };
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// const firestore = getFirestore(app);
-// const storage = getStorage(app);
+// Import the functions you need from the SDKs you need
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-// export { app, auth, firestore, storage };
+const firebaseConfig = {
+  apiKey: "AIzaSyBSFxe544o87EAOxOLo0cQT24QyMgW9JQc",
+  authDomain: "insta-clone-756af.firebaseapp.com",
+  projectId: "insta-clone-756af",
+  storageBucket: "insta-clone-756af.appspot.com",
+  messagingSenderId: "820026503310",
+  appId: "1:820026503310:web:422147c182fc65c494d9df",
+  measurementId: "G-F64DVQQEB9",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, firestore, storage };
+
+
+
+
+// cloud firestore example
+// rules_version = '2';
+
+// service cloud.firestore {
+//   match /databases/{database}/documents {
+//   match /users/{userId}{
+//   allow read;
+//   allow write: if request.auth != null && request.auth.uid == userId;
+//   }
+//   match/posts/{postId}{
+//  allow read;
+//  allow create: if request.auth != null;
+//  allow update: if request.auth != null && request.auth.uid == resource.data.createdBY;
+//  allow delete: if request.auth != null && request.auth.uid == resource.data.createdBY;
+// }
+
+//   }
+// }
